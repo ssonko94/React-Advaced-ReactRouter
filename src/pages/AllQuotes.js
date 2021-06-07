2532500ab0cde4f93a14a230c64b3c193e86546d
ssonko94 <ssonkojimmy@gmail.com>
2021-06-07 18:56:58 +0300

    use the custom hook with useEffect to load qoutes from firebase

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 import QuoteList from "../components/quotes/QuoteList";
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
 
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Dan", text: "The world is a savage place" },
-  { id: "q2", author: "Jimmy", text: "It will end in tears" },
-  { id: "q3", author: "Bobi", text: "Buli omu agumye mune" },
-];
+// const DUMMY_QUOTES = [
+//   { id: "q1", author: "Dan", text: "The world is a savage place" },
+//   { id: "q2", author: "Jimmy", text: "It will end in tears" },
+//   { id: "q3", author: "Bobi", text: "Buli omu agumye mune" },
+// ];
 
 const AllQuotes = () => {
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (status === "completed" && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
   return (
     <>
-      <QuoteList quotes={DUMMY_QUOTES} />
+      <QuoteList quotes={loadedQuotes} />
     </>
   );
 };
