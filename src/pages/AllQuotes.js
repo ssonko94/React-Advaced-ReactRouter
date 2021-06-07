bf5e8c6d20eeab280467a5274802e60a03f8b3dd
ssonko94 <ssonkojimmy@gmail.com>
2021-06-07 06:35:56 +0300

    add an all quotes page

---
+++ src/pages/AllQuotes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MainHeader from "../components/layout/MainHeader";
+import Card from "../components/UI/Card";
+// import QouteList from "../components/quotes/QuoteList";
+
+const AllQuotes = () => {
+  return (
+    <>
+      <MainHeader />
+      <Card>{/* <QouteList /> */}</Card>
+    </>
+  );
+};
+
+export default AllQuotes;
