f264eeab6b29f8d8c2132fd5ffe716f52942864e
ssonko94 <ssonkojimmy@gmail.com>
2021-06-07 12:02:18 +0300

    add some dummy quotes to render a list of quotes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,12 +1,27 @@
 import React from "react";
-import MainHeader from "../components/layout/MainHeader";
+import { useParams, Route } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Dan", text: "The world is a savage place" },
+  { id: "q2", author: "Jimmy", text: "It will end in tears" },
+  { id: "q3", author: "Bobi", text: "Buli omu agumye mune" },
+];
 
 const QuoteDetail = () => {
+  const params = useParams();
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found 😥</p>;
+  }
   return (
     <>
-      <MainHeader />
-      
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
     </>
   );
 };
