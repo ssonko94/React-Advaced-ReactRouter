93a198f14a11e42b38c1ecc55db6e272fbbb8de8
ssonko94 <ssonkojimmy@gmail.com>
2021-06-07 18:56:06 +0300

    use the custom useHttp hook to add a new quote to the server database

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,19 +1,26 @@
-import React from "react";
-import { useHistory } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 import QuoteForm from "../components/quotes/QuoteForm";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 const AddNewQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
-  const addQuoteHandler = (quoteData) => {
-    
-    console.log(quoteData);
 
-    history.push('/quotes')
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
+  const addQuoteHandler = (quoteData) => {
+    sendRequest(quoteData);
   };
   return (
     <>
-      <QuoteForm onAddQuote={addQuoteHandler} />
+      <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler} />
     </>
   );
 };
