687955408bd3a983ce9089e208c8e1b0562b4b87
ssonko94 <ssonkojimmy@gmail.com>
2021-06-07 06:37:00 +0300

    add a main header component

---
+++ src/components/layout/MainHeader.js
@@ -0,0 +1,27 @@
+import React from "react";
+import classes from "./MainNavigation.module.css";
+import { NavLink } from "react-router-dom";
+
+const MainHeader = () => {
+  return (
+    <header className={classes.header}>
+      <div className="logo">GREAT QUOTES</div>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <NavLink activeClassName={classes.active} to="/quotes">
+              All Quotes
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName={classes.active} to="/new-quote">
+              Add a Quote
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
