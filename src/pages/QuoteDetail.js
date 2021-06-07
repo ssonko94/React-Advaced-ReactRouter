361009fe331242ce53d1ca12b594459a79af71fa
ssonko94 <ssonkojimmy@gmail.com>
2021-06-07 06:35:00 +0300

    add a quote details page

---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MainHeader from "../components/layout/MainHeader";
+
+
+const QuoteDetail = () => {
+  return (
+    <>
+      <MainHeader />
+      
+    </>
+  );
+};
+
+export default QuoteDetail;
