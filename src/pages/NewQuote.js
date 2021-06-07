f5719530e7e7c3a9bfd7c8d1b4d6a0be9761806a
ssonko94 <ssonkojimmy@gmail.com>
2021-06-07 06:35:28 +0300

    add a new quote page

---
+++ src/pages/NewQuote.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MainHeader from "../components/layout/MainHeader";
+import Card from "../components/UI/Card";
+
+const AddNewQuote = () => {
+  return (
+    <>
+      <MainHeader />
+      <Card></Card>
+    </>
+  );
+};
+
+export default AddNewQuote;
