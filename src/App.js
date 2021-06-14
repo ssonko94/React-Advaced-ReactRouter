78da3d8180dac57458ee9f433d0b7dcbd0a1a71f
ssonko94 <ssonkojimmy@gmail.com>
2021-06-14 06:55:51 +0300

    remove the exact prop in order to load various quote ids

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
 import Layout from "./components/layout/Layout";
-import NotFound from "./pages/NotFound"
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -15,14 +15,14 @@ function App() {
         <Route path="/quotes" exact>
           <AllQuotes />
         </Route>
-        <Route path="/quotes/:quoteId" exact>
+        <Route path="/quotes/:quoteId" >
           <QuoteDetail />
         </Route>
         <Route path="/new-quote" exact>
           <NewQuote />
         </Route>
         <Route path="*">
-          <NotFound/>
+          <NotFound />
         </Route>
       </Switch>
     </Layout>
